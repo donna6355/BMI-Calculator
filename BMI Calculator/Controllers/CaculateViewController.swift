ac7fc4ad5cc6b331f2786eec83365d2815297a27
Donna <donnajun1214@gmail.com>
2022-05-25 15:08:22 +0900

    how to navigate via segue

--- BMI Calculator/Controllers/CaculateViewController.swift
+++ BMI Calculator/Controllers/CaculateViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CaculateViewController: UIViewController {
     @IBOutlet weak var weightLabel: UILabel!
     @IBOutlet weak var heightLabel: UILabel!
 
     @IBOutlet weak var weightSlider: UISlider!
     @IBOutlet weak var heightSlider: UISlider!
+    
+    var bmiValue = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,10 +37,16 @@ class ViewController: UIViewController {
         let weight = weightSlider.value
         
         let bmi = weight / pow(height,2)
-        print(bmi)
-        let secondVC = SecondViewController()
-        secondVC.bmiValue = String(format: "%.1f", bmi)
-        self.present(secondVC, animated: true, completion: nil)
+        bmiValue = String(format:"%.1f",bmi)
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+        
+ 
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult"{
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = bmiValue
+        }
     }
 }
 
