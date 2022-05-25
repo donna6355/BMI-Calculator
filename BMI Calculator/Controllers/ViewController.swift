5cadb1ceabefa67fc2ebb16fea699cbc6d8815ac
Donna <donnajun1214@gmail.com>
2022-05-25 13:47:58 +0900

    add IBOutlet and functionalities

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
 
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var heightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+//        heightLabel.text = "\(round(sender.value * 10) / 10.0)m"
+        heightLabel.text = "\(String(format: "%.2f", sender.value))m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = "\(Int(sender.value))kg"
+    }
+    @IBAction func caculatePressed(_ sender: UIButton) {
+        //caculate bmi = weight / height * height
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight / pow(height,2)
+        print(bmi)
+    }
 }
 
